f355a3868de91f5e8f508a5c68b94afc46870933
Yanael Barbier <y.barbier@sprout.ai>
2024-10-05 13:34:37 -0700

    add code exec

---
+++ code_exec.py
@@ -0,0 +1,20 @@
+import os
+from e2b_code_interpreter import CodeInterpreter
+
+
+BROWSERBASE_API_KEY = os.getenv("BROWSERBASE_API_KEY")
+BROWSERBASE_PROJECT_ID = os.getenv("BROWSERBASE_PROJECT_ID")
+
+assert BROWSERBASE_API_KEY
+assert BROWSERBASE_PROJECT_ID
+
+code = "print('Hello, World!')"
+
+with CodeInterpreter(template="e2b-hack", 
+    env_vars={
+        "BROWSERBASE_API_KEY": BROWSERBASE_API_KEY,
+        "BROWSERBASE_PROJECT_ID": BROWSERBASE_PROJECT_ID,
+    }) as sandbox:
+    execution = sandbox.notebook.exec_cell("print('hello')")
+
+    print(execution)
